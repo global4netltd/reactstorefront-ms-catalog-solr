5dd733cb52a5cb27fee39ca78e30b82df018419d
Choinek <adrian@chojnicki.pl>
2019-07-11 17:46:52 +0200

    + abstract field + solr field

---
+++ src/Document/Field.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace G4NReact\MsCatalogSolr\Document;
+
+use G4NReact\MsCatalog\Document\AbstractField;
+
+/**
+ * Class Field
+ * @package G4NReact\MsCatalog\Document
+ */
+class Field extends AbstractField
+{
+    public function getFieldName()
+    {
+        // TODO: Implement getFieldName() method.
+    }
+}

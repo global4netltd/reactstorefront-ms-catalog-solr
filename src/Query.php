f755e91ad5210dbab50d219db6ca71d9048bd75d
norbert <n.malecki@global4net.com>
2019-07-11 14:13:35 +0200

    add Solr Query

--- src/Query.php
+++ src/Query.php
@@ -3,7 +3,9 @@
 namespace G4NReact\MsCatalogSolr;
 
 use G4NReact\MsCatalog\AbstractQuery;
+use G4NReact\MsCatalog\Document\Field;
 use Solarium\Core\Query\Result\ResultInterface;
+use Solarium\QueryType\Select\Query\Query as SolariumSelectQuery;
 
 /**
  * Class Query
@@ -11,18 +13,31 @@ use Solarium\Core\Query\Result\ResultInterface;
  */
 class Query extends AbstractQuery
 {
-    /** text, sort, filter, page size, query text */
+    /**
+     * @var SolariumSelectQuery
+     */
+    protected $query;
+
     /**
      * @return ResultInterface
      */
-    public function buildQuery()
+    public function buildQuery() : ResultInterface
     {
         /** @var \G4NReact\MsCatalogSolr\Client\Client $client */
         $client = $this->getClient();
+
         $query = $client
             ->getSelect()
-            ->setFields($this->fields)
+            ->setQuery($this->getQueryText())
+            ->setFilterQueries($this->prepareFilterQueries())
+            ->setFields($this->prepareFields())
+            ->setStart($this->getPageStart())
+            ->setRows($this->getPageSize())
             ->addSorts($this->sort);
+
+        $this->query = $query;
+        $this->addFacetsToQuery();
+        $this->addStatsToQuery();
 
         return $client->query($query);
     }
@@ -30,5 +45,54 @@ class Query extends AbstractQuery
     public function getResponse()
     {
         // TODO: Implement getResponse() method.
+    }
+
+    /**
+     * @return array
+     */
+    protected function prepareFilterQueries() : array
+    {
+        $filtersQuery = [];
+
+        foreach ($this->filters as $key => $filter) {
+            if (!isset($filter[self::FIELD]) || !isset($filter[self::NEGATIVE])) {
+                continue;
+            }
+            /** @var Field $field */
+            $field = $filter[self::FIELD];
+            $filtersQuery[$key] = $filter[self::NEGATIVE] ? -$field->getValue() : $field->getValue();
+        }
+
+        return $filtersQuery;
+    }
+
+    /**
+     * Add Facets to Query
+     */
+    protected function addFacetsToQuery()
+    {
+        $this->query->getFacetSet()->addFacets($this->facets);
+    }
+
+    /**
+     * Add Stats to Query
+     */
+    protected function addStatsToQuery()
+    {
+        $this->query->getStats()->addFacets($this->facets);
+    }
+
+    /**
+     * @return array
+     */
+    protected function prepareFields() : array
+    {
+        $fields = [];
+        /** @var Field $field */
+        foreach ($this->fields as $field) {
+            $fields [] = $field->getValue();
+        }
+
+        return $fields;
     }
 }
